e755c0704cb5d4630fa8f7b96eaeb32a80bc72b7
hugob <hugo.bourdais@reseau.eseo.fr>
2025-01-06 17:09:01 +0100

    update: event card

--- lib/widgets/google_auth_button.dart
+++ lib/widgets/google_auth_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../pages/connexion_page.dart';
 import '../pages/welcome_page.dart';
 
-bool isAuthenticated = userName!='';
+bool isAuthenticated = userName != '';
 
 class GoogleAccountToggle extends StatefulWidget {
   const GoogleAccountToggle({super.key});
@@ -12,7 +12,7 @@ class GoogleAccountToggle extends StatefulWidget {
 }
 
 class _GoogleAccountToggleState extends State<GoogleAccountToggle> {
-   // Détermine si l'utilisateur est authentifié ou non
+  // Détermine si l'utilisateur est authentifié ou non
 
   // Méthode pour simuler l'authentification
   void _toggleAuthentication() {
@@ -25,16 +25,12 @@ class _GoogleAccountToggleState extends State<GoogleAccountToggle> {
       });
     } else {
       setState(() {
-
-
-
         login().then((value) => setState(() {
-          isAuthenticated = value;
-        }));
-    });
-          }
-      }
-
+              isAuthenticated = value;
+            }));
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,8 +75,11 @@ class _GoogleAccountToggleState extends State<GoogleAccountToggle> {
         // Bouton sous forme de logo (icône d'authentification)
         IconButton(
           icon: Icon(
-            isAuthenticated ? Icons.check_circle_outlined : Icons.cancel_outlined, // Change l'icône en fonction de l'état
-            color: isAuthenticated ? Colors.green : Colors.red, // Couleur de l'icône
+            isAuthenticated
+                ? Icons.check_circle_outlined
+                : Icons.cancel_outlined, // Change l'icône en fonction de l'état
+            color: isAuthenticated ? Colors.green : Colors.red,
+            // Couleur de l'icône
             size: 30, // Taille de l'icône
           ),
           onPressed: _toggleAuthentication, // Méthode appelée au clic
